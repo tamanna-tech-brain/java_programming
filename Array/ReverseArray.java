8133ce891eaeeb1d16ebfc0a0647387156b288c4
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-13 00:34:17 +0530

    array

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -7,21 +7,18 @@ public class ReverseArray {
         int n = sc.nextInt();
         System.out.print("Elements of array i: ");
         int[] arr = new int[n];
-        for(int i =0; i<n; i++){
+        int i;
+        for(i =0; i<n; i++){
             arr[i]= sc.nextInt();
         }
-        int i =0, j = n-1;
-        while(i<j){
-            int temp = arr[i];
-            arr[i] = arr [j];
-            arr[j] = temp;
-            i++;
-            j--;
-        }
-        for(int ele :arr){
-            System.out.print(ele+" ");
+        for( i =0; i<n/2; i++){
+                int temp = arr[i];
+                arr[i] = arr[n-1-i];
+                arr[n-1-i] = temp;
+            }
+            for( i =0; i<n; i++){
+                System.out.print(arr[i] + " ");
+            }
         }
         
     }
-
-}
