9be6322bcd2c5b555c410f453c9eb7cc4db08dd9
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-14 14:12:31 +0530

    Q

---
+++ Practise/Question/Q6.java
@@ -0,0 +1,15 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q6 {
+     public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum =0;
+        for(int i = 1; i<=n; i++){
+            sum = sum +i;
+        }
+        System.out.print(sum+ " ");
+    }
+}
