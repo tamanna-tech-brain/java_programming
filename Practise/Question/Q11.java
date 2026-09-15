f35b3d63d5e3c847a69e05f8501b7fcc00f2699c
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-15 19:35:19 +0530

    Q

---
+++ Practise/Question/Q11.java
@@ -0,0 +1,15 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int i = 7 - x- y;
+		    System.out.println(i);
+	}
+}
