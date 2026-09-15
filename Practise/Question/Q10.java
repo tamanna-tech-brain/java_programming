69781c80da07ec7317be13cc659887837a553757
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-15 19:34:09 +0530

    Q

---
+++ Practise/Question/Q10.java
@@ -0,0 +1,14 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q10 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        String S  = sc.next();
+        System.out.println(S.charAt(6));
+        
+	}
+}
