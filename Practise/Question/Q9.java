f19b71126b6ae48780673fd0390679bb9ce12a30
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-15 19:32:50 +0530

    Q

---
+++ Practise/Question/Q9.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q9
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if(x >= 12 ){
+		    System.out.print("yes");
+		}
+		else{
+		    System.out.print("No");
+		}
+
+	}
+}
