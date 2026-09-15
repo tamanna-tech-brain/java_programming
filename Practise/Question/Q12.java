295fee8423784d3ad60b0364a9a6da657c88dfd5
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-15 19:36:16 +0530

    Q

---
+++ Practise/Question/Q12.java
@@ -0,0 +1,13 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q12 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(2*x);
+	}
+}
