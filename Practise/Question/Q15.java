cf14fc3cefed57f9d602cce33283be2f14ea5547
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-16 19:09:23 +0530

    Q

---
+++ Practise/Question/Q15.java
@@ -0,0 +1,21 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q15 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i =0; i<T; i++){
+            int x = sc.nextInt();
+            if(x == 6 ){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+	}
+}
