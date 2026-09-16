b0a3b18531e6f39c4aaa0372ae8a85463f3362ba
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-16 19:11:24 +0530

    Q

---
+++ Practise/Question/Q16.java
@@ -0,0 +1,14 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q16 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x-y);
+	}
+}
