170859e80528d83037e0c21fe9e024038dde10f9
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-16 19:13:45 +0530

    Q

---
+++ Practise/Question/Q18.java
@@ -0,0 +1,17 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q18 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i =0; i<T; i++){
+            int x = sc.nextInt();
+            int N = sc.nextInt();
+                System.out.println((x /10)*N);
+        }
+    }
+}
---
+++ Practise/Question/Q19.java
@@ -0,0 +1,14 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q19 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+            System.out.println(A+B+"1");
+	}
+}
