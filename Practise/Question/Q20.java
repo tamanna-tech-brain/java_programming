3dc912da0cbbad60ea9bd6456f710de9d0095291
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-16 19:15:47 +0530

    Q

---
+++ Practise/Question/Q20.java
@@ -0,0 +1,17 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q20 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i =0; i<T; i++){
+            int N= sc.nextInt();
+            int X =sc.nextInt();
+            System.out.println(N-X);
+        }
+	}
+}
---
+++ Practise/Question/Q21.java
@@ -0,0 +1,17 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q21 {
+    	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i = 0; i< T; i++){
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(-(x-y));
+        }
+	}
+}
