2b87640d9917522b1b1e65b3a78d538f2e6c9fd8
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-17 17:43:52 +0530

    Q

---
+++ Practise/Q28.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q28 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+                int X = sc.nextInt();
+                int Y = sc.nextInt();
+                System.out.println((N*X)+(M*Y));
+        }
+}
