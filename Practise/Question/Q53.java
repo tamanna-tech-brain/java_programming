bd54e8ee283aedb863b9f96192f5f0a2f793dea8
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-22 19:54:33 +0530

    Q

---
+++ Practise/Question/Q53.java
@@ -0,0 +1,18 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q53 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =0; i<T; i++){
+		    int N = sc.nextInt();
+		    int X = sc.nextInt();
+		            System.out.println(Math.min(X,N-X));
+		}
+
+	}
+}
