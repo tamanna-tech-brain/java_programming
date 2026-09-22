20c5b0e8d968dec43c70b366da81856d541fe341
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-22 19:59:21 +0530

    Q

---
+++ Practise/Question/Q55.java
@@ -0,0 +1,20 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q55 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =0; i<T; i++){
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    int Z = 2*Y;
+		        System.out.println(X/Z);
+	
+            }
+
+	}
+}
