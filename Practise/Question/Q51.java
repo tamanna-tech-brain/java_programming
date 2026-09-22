34c4e5e8a143718cdee4303494d9ed7daff1661c
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-22 18:25:15 +0530

    Q

---
+++ Practise/Question/Q51.java
@@ -0,0 +1,17 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q51 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int Z = y/2;
+		int t= x-y;
+		System.out.println(Z+t);
+
+	}
+}
