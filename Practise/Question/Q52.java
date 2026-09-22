fc509708ff2223f91ee901413042aeabb91d0b21
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-22 19:52:29 +0530

    Q

---
+++ Practise/Question/Q52.java
@@ -0,0 +1,18 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q52 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =0; i<T; i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		        System.out.println(Math.abs(x-y));
+		}
+
+	}
+}
