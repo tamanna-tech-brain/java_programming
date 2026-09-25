f7bf1d2a22dc9a9c52bd35e6eca48820f838fae3
Tamanna Verma <tamannaverma7389@gmail.com>
2026-09-25 21:44:01 +0530

    Q

---
+++ Practise/Question/Q71.java
@@ -0,0 +1,18 @@
+package Question;
+
+import java.util.Scanner;
+
+public class Q71 {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =0; i<T; i++){
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    System.out.println(X/Y);
+		}
+
+	}
+}
